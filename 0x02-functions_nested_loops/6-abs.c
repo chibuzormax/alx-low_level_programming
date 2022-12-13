6c74e23771cc68dd32103b342002daceac29cd9c
Maximus Isaac <chibuzormax@gmail.com>
2022-12-13 14:52:55 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - A function that computesabs of a no.
+ * @n: the int to check
+ * Return: 0 if successful
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
