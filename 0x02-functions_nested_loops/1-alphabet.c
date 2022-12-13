0f7d04b6d8a8f2043fbf57cfe127c33e009ec723
Maximus Isaac <chibuzormax@gmail.com>
2022-12-13 08:36:11 -0800

    - 1. Printing of alphabets in lowercase task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+		_putchar(lower);
+	_putchar('\n');
+}
