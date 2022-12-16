8cec1a107d82a57de97827395e2b423482263a16
Maximus Isaac <chibuzormax@gmail.com>
2022-12-15 17:57:28 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ * @n: number of _ to be printed
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+		_putchar('_');
+	_putchar('\n');
+}
