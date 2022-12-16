c8ed54c4a4e375e26f366116a1e9009311da42a3
Maximus Isaac <chibuzormax@gmail.com>
2022-12-16 01:24:59 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,12 +7,17 @@
 
 void print_diagonal(int n)
 {
-	while (n-- > 0)
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
 	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
 		_putchar('\\');
 		_putchar('\n');
-		_putchar(' ');
 	}
-	_putchar(' ');
-	_putchar('\n');
 }
