7ab26b6c3800d854cbd7c681587ee2348a0a16be
Maximus Isaac <chibuzormax@gmail.com>
2022-12-16 13:05:04 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle with # character
+ * @size: number of # to print
+ */
+
+void print_triangle(int size)
+{
+	int r, h, s;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+		for (r = 1; r <= size; r++)
+		{
+			for (s = size - r; s >= 1; s++)
+				_putchar(' ');
+			for (h = 1; h <= r; h++)
+				_putchar('#');
+			_putchar('\n');
+		}
+}
