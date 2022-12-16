01b840ec0eb279ad1cfb68197353af9ed24bc34a
Maximus Isaac <chibuzormax@gmail.com>
2022-12-15 22:48:08 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,13 +6,15 @@
 
 void more_numbers(void)
 {
-	char c;
+	int n;
 	int count = 0;
 
 	while (count++ <= 10)
 	{
-		for (c = '0'; c <= '14'; c++)
-			_putchar(c);
+		for (n = 0; n <= 14; n++)
+			if (n >= 10)
+				_putchar('n');
+			_putchar(n % 10 + '0');
 		_putchar('\n');
 	}
 }
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,7 +7,9 @@
 
 void print_line(int n)
 {
-	if (n <= 0)
+	if (n-- > 0)
+		_putchar('_');
+	else if (n >= 0)
 		_putchar('\n');
 	else
 		_putchar('_');
