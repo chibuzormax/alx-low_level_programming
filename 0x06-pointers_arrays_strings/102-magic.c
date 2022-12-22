ec26d65c4411f5215ba32ce20b986522657c40ce
Maximus Isaac <chibuzormax@gmail.com>
2022-12-22 12:53:07 -0800

    102-magic.c task

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - magic function
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int n, a[5], *p;
+
+	a[2] = 1024;
+	p = &n;
+
+	*(p + 5) = 98;
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
