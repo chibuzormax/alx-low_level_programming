cb3350a35894b56e785f6ae2c12e045a2532cc17
Maximus Isaac <chibuzormax@gmail.com>
2023-01-10 00:36:35 -0800

    Arguments task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Adas positive integers together
+ * @argc: Arguments count
+ * @argv: Arguments vector
+ *
+ * Return: 0 Always
+ */
+
+int main(int argc, char **argv)
+{
+	int i, n, sum = 0;
+	char *flag;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 0; argv[i]; i++)
+	{
+		n = strtol(argv[i], &flag, 10);
+		if (*flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			sum += n;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
