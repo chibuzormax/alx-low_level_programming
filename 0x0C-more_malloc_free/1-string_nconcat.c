40ae9726bc9b752c2401c9dc11851208face860f
Maximus Isaac <chibuzormax@gmail.com>
2023-01-13 13:11:20 -0800

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,12 +17,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 		x = 0;
 	else
-		for (x = 0; s1[x]; ++x)
+		for (x = 0; s1[x]; x++)
 			;
 	if (s2 == NULL)
 		y = 0;
 	else
-		for (y = 0; s2[y]; ++y)
+		for (y = 0; s2[y]; y++)
 			;
 	if (y > n)
 		y = n;
