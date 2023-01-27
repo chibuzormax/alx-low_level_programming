853be0e61495a90d8f3dfdd524bb321d94596cc1
Maximus Isaac <chibuzormax@gmail.com>
2023-01-27 11:10:36 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * myStartupFun - Apply the constructor attribute to myStartupFunc()
+ * so that it is executed before main()
+ */
+
+void myStartupFun(void) __attribute__ ((constructor));
+
+/**
+ * myStartupFun - implementation of myStartupFun
+ */
+
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
